336782172433d369fcd597d4077b2d9e320cd009
Max3189526 <maxduyc@gmail.com>
2026-09-11 19:18:46 -0700

    change player.cs
    enabled player move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,32 @@ using UnityEngine.Events;
 public class Player : MonoBehaviour
 {
     //Add Variables here
-  
+    float speed = 10.0f;
+    public UnityEvent gameEnd;
     // Update is called once per frame
     void Update()
     {
         //Add Movement checking here
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += UnityEngine.Vector3.left * Time.deltaTime * speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += UnityEngine.Vector3.right * Time.deltaTime * speed;
+        }
 
     }
 
     //Add OnCollisionEnter() here
-
+    void OnCollisionEnter(Collision Other)
+    {
+        if (Other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            gameEnd.Invoke();
+        }
+    }
 
 }
